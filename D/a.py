5173b85f276200768c3d10a8991b1abec3a551b4
akirovab <57772695+akirovab@users.noreply.github.com>
2021-04-19 14:33:30 +0500

    Add files via upload

---
+++ D/a.py
@@ -0,0 +1,6 @@
+l = [4,5, 3, 4, 2, 3]
+i = 0
+while i < len(l):
+    if l[i] % 2 == 0:
+        print(l[i])
+    i += 1
---
+++ D/c.py
@@ -0,0 +1,9 @@
+p = 0
+a = []
+for i in range(int(input())):
+    n = int(input())
+    a.append(n)
+    if n > 0:
+        p += 1
+print(a)
+print(p)
